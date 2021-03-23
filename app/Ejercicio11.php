bacf66ca84a7e376559d5d8a14984eb560c5df4b
Daniel <danielmarinicrituss@gmail.com>
2021-03-23 20:10:31 -0300

    AgregoEjercicio12

---
+++ app/Ejercicio11.php
@@ -0,0 +1,14 @@
+<?php
+/*Aplicación No 11 (Potencias de números)
+Mostrar por pantalla las primeras 4 potencias de los números del uno 1 al 4 (hacer una función
+que las calcule invocando la función pow).
+ Marini Daniel.-
+*/
+
+
+
+	
+?>
+	
+
+
---
+++ app/Ejercicio12.php
@@ -0,0 +1,25 @@
+<?php
+/*Aplicación No 12 (Invertir palabra)
+Realizar el desarrollo de una función que reciba un Array de caracteres y que invierta el orden
+de las letras del Array.
+Ejemplo: Se recibe la palabra “HOLA” y luego queda “ALOH”.
+ Marini Daniel.-
+*/
+
+$cadena = "HOLA";
+
+printf("Comun : <br>");
+echo $cadena;
+
+echo "<br>";
+printf("\nInvertido : <br>");
+
+$invertir=strrev($cadena);
+
+echo $invertir;
+
+
+?>
+	
+
+
